c56ca8b13fa63996572aa8504f9eb47b75d7f750
Nanditha Nabiilah Putri <98955945+nandithanabiilahp@users.noreply.github.com>
2022-02-06 18:40:37 +0700

    Tugas SDA Nanditha Nabiilah Putri_G1A021001
    Program Menghitung Luas dan Keliling Lingkaran
    Nama : Nanditha Nabiilah Putri
    NPM : G1A021001
    Kelas : A
    
    Penjelasan Source Code :
    
    -#include adalah sebuah prosesor pengarah yang mengatakan kepada kompiler untuk meletakan kode dari header file iostream kedalam program.
    
    -using namespace std adalah sebuah instruksi terhadap kompiler untuk menggunakan semua fungsi yang terkait untuk kerangka yang sama, bisa berupa berkas/file , class, dan sejenisnya yang berkaitan dengan std.
    
    -int main() merupakan fungsi utama, fungsi yang akan dibaca oleh kompilator pertama kali secara otomatis, tidak akan ada function yang otomatis dibaca oleh kompilator kecuali fungsi utama int main(), fungsi utama bagaikan sebuah kepala dari sebuah program yang mengatur arah kompilator.
    
    -{ } sebagai blok statement, menyatukan dua atau lebih statement menjadi satu statement.
    
    -float mengembalikan bilangan ke dalam bentuk float (bilangan dengan titik). Jadi fungsi ini akan mengubah argumennya yang berupa bilangan menjadi tipe float atau bilangan desimal.
    
    -L, K, r, d adalah variabel.
    
    -const mendefinisikan sebuah variable yang nilainya tak dapat di rubah atau sudah di tetapkan oleh user. Const adalah cara yang di siapkan untuk mendefinisikan konstanta dalam C++.
    
    -cout (character out) dipakai untuk menampilkan text di layar monitor anda. Cout merupakan sebuah objek di dalam C++, yang digunakan untuk mengarahkan data ke dalam standar output (cetak pada layar).
    
    -cin (c input) adalah fungsi untuk mengambil input dari keyboard. Fungsi cin membutuhkan variabel untuk menyimpan data yang diinputkan.
    
    -Tanda <<, yang diketahui sebagai operator pemasukan. Tanda tersebut mengatakan kepada kompiler agar segera menghasilkan output sesuai dengan input anda.
    
    -Tanda >>, yang diketahui sebagai operator pengeluaran. Tanda tersebut mengatakan kepada kompiler agar segera menghasilkan input sesuai dengan output anda.
    
    -Tanda “  ” adalah untuk membuat sebuah string literal.
    
    -\n adalah simbol untuk membuat baris baru.
    
    -\t adalah simbol untuk horizontal tab ( teks akan bergeser ke kanan sebagai pengganti tab)..
    
    -endl digunakan untuk mengakhiri baris/pindah baris pada hasil layar program.
    
    -; adalah untuk memberi tau komputer bahwa garis perintah pada pemrograman sudah mencapai titik akhir perintahnya.
    
    -* simbol perkalian.
    
    -( ) digunakan untuk mengapit argumen fungsi, yaitu nilai yang dilewatkan ke fungsi. Pada fungsi main( ) tidak ada argumen yang diberikan, maka tidak ada entri di dalam ( ).
    
    -return 0 digunakan untuk memberitahu bahwa kode telah selesai di eksekusi dengan exit kode 0.

---
+++ Nandithaaa.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  float L, K, r, d;
+  const float phi = 3.14;
+  
+  cout << "=================================\n";
+  cout << "Menghitung Luas Lingkaran\n";
+  cout << "Menghitung Keliling Lingkaran\n";
+  cout << "=================================\n\n";
+
+  cout << "Masukkan panjang jari-jari lingkaran \t: ";
+  cin >> r;
+  cout << "Masukkan panjang diameter lingkaran \t: ";
+  cin >> d;
+
+  L = phi * r * r;
+  K = phi * d;
+  
+  cout << "\n";
+  
+  cout << "Luas lingkaran : " << endl;
+  cout << "L = phi x r x r = (" << phi << " x " << r<< " x "<< r <<")" << endl;
+  cout << "L = " << L << " cm " << endl;
+  cout << "Jadi, luas lingkaran adalah " << L << " cm " << endl;
+  
+  cout << "\n";
+  
+  cout << "Keliling lingkaran : " << endl;
+  cout << "K = phi x d = (" << phi<< " x " << d << ")" << endl;
+  cout << "K = " << K << " cm " << endl;
+  cout << "Jadi, keliling lingkaran adalah " << K << " cm " << endl;
+
+  return 0;
+}
